12041503166e037af15d82706eda287d5ba880eb
Bogdana Vereha <bogdana.vereha@gmail.com>
2018-08-23 18:07:00 +0200

    Create span without context and include all fields

--- server.rb
+++ server.rb
@@ -16,12 +16,12 @@ def s3
     @s3 ||= Aws::S3::Client.new
 end
 
-def process_span(job_id, span_context, trace_json)
-    span = span_context.start_span trace_json['name'], skip_frames: 2
-    span.put_attribute "app", "build"
-    span.put_attribute "job_id", job_id
-    span.start_time = Time.parse(trace_json['start_time'])
-    span.end_time = Time.parse(trace_json['end_time'])
+def process_span(job_id, trace_id, trace_json)
+    builder = OpenCensus::Trace::SpanBuilder::PieceBuilder.new 
+    span_name = builder.truncatable_string(trace_json['name'])
+    status = builder.convert_status(trace_json['status'], "")
+    attributes = builder.convert_attributes({"app"=>"build_test", "job_id"=>job_id})
+    return OpenCensus::Trace::Span.new trace_id, trace_json['id'], span_name, Time.at(trace_json['start_time'].to_i*1e-9), Time.at(trace_json['end_time'].to_i*1e-9), parent_span_id: trace_json['parent_id'], attributes: attributes, status: status
 end
 
 def s3_trace(job_id)
@@ -56,14 +56,9 @@ post '/trace' do
     max_trace_id = OpenCensus::Trace::SpanContext::MAX_TRACE_ID
     trace_id = rand 1..max_trace_id
     trace_id = trace_id.to_s(16).rjust(32, "0")
-    context = OpenCensus::Trace::TraceContextData.new(trace_id, '', 0x01)
-
-    OpenCensus::Trace.start_request_trace \
-    trace_context: context,
-    same_process_as_parent: false do |span_context|
-        tracefile.each_line do |line|
-            process_span(job_id, span_context, JSON.parse(line))
-        end
-        OpenCensus::Trace.config.exporter.export(span_context.build_contained_spans)
+    spans = []
+    tracefile.each_line do |line|
+        spans << process_span(job_id, trace_id, JSON.parse(line))
     end
+    OpenCensus::Trace.config.exporter.export spans
 end
