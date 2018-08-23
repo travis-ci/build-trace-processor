871cdabf9337358a723de77a39da659dd1e308e1
Bogdana Vereha <bogdana.vereha@gmail.com>
2018-08-23 15:19:33 +0200

    Fix span creation and export

--- server.rb
+++ server.rb
@@ -16,19 +16,12 @@ def s3
     @s3 ||= Aws::S3::Client.new
 end
 
-def process_trace(job_id, span_context, trace_json)
-    begin
-        span_context.in_span trace_json['name'] do |span|
-            span.kind = ::OpenCensus::Trace::SpanBuilder::SERVER
-            span.put_attribute "app", "build"
-            span.put_attribute "job_id", job_id
-            span.start_time = Time.parse(trace_json['start_time'])
-            span.end_time = Time.parse(trace_json['end_time'])
-        end
-    rescue  => error
-        # This is needed because the block above raises "Span already finished"
-        raise error if error.message != "Span already finished"
-    end
+def process_span(job_id, span_context, trace_json)
+    span = span_context.start_span trace_json['name'], skip_frames: 2
+    span.put_attribute "app", "build"
+    span.put_attribute "job_id", job_id
+    span.start_time = Time.parse(trace_json['start_time'])
+    span.end_time = Time.parse(trace_json['end_time'])
 end
 
 def s3_trace(job_id)
@@ -57,20 +50,20 @@ post '/trace' do
     end
 
     job_id = json_params["job_id"]
-    #tracefile = s3_trace(job_id)
-    tracefile = File.open("trace.json", "r")
+    tracefile = s3_trace(job_id)
 
     # create new trace
     max_trace_id = OpenCensus::Trace::SpanContext::MAX_TRACE_ID
     trace_id = rand 1..max_trace_id
     trace_id = trace_id.to_s(16).rjust(32, "0")
     context = OpenCensus::Trace::TraceContextData.new(trace_id, '', 0x01)
+
     OpenCensus::Trace.start_request_trace \
     trace_context: context,
     same_process_as_parent: false do |span_context|
         tracefile.each_line do |line|
-            process_trace(job_id, span_context, JSON.parse(line))
+            process_span(job_id, span_context, JSON.parse(line))
         end
-        OpenCensus::Trace.config.exporter.export span_context.build_contained_spans
+        OpenCensus::Trace.config.exporter.export(span_context.build_contained_spans)
     end
 end
